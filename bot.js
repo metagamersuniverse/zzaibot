a235b0094ec824c5862b7d1740b6e8cb2b154180
metagamersuniversE <metagamersuniverse@gmail.com>
2023-06-10 02:50:38 +0600

    aaa

--- bot.js
+++ bot.js
@@ -13,11 +13,13 @@ const imagebot = new TelegramClient('imagebot', {
 });
 
 imagebot.on(events.NewMessage({ pattern: /^[?!/]start/ }), async (event) => {
+  console.log('Received /start command');
   const welcomeMessage = "Hi! Welcome to AI POP bot. Send me a photo and I'll generate a description for you. Type 'help' if you need assistance. Let's get started!";
   await event.reply(welcomeMessage);
 });
 
 imagebot.on(events.NewMessage({ pattern: /^[?!/]help/ }), async (event) => {
+  console.log('Received /help command');
   const helpMessage = "Hi! I'm here to help you with the bot.\n\nTo get started, simply send me a photo and I'll generate a description for you.\n\nIf you need additional assistance, you can check out our guide at this link: https://docs.ai-pop.com/guides/ai-pop-bot-use\n\nIf you have any questions or need further assistance, feel free to ask in chat!";
   await event.reply(helpMessage);
 });
@@ -27,6 +29,7 @@ imagebot.on(events.NewMessage({ incoming: true }), async (event) => {
     return;
   }
   if (!event.media) {
+    console.log('Received message without media');
     await event.reply("```Don't send just text please. Please send an image.```");
     return;
   }
@@ -38,6 +41,7 @@ imagebot.on(events.NewMessage({ incoming: true }), async (event) => {
     task: "image_captioning",
   };
   const output = await version.predict(inputs);
+  console.log('Image caption generated:', output);
   await event.reply(output);
   let result = output.caption;
   result = result.replace("Caption: ", "");
